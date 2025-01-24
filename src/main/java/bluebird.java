cf970a89e11380cfee2200aaf19cfbe801d1ed22
zavsky <irwinteo@gmail.com>
2025-01-24 12:23:51 +0800

    Rename, add greet, exit

---
+++ src/main/java/bluebird.java
@@ -0,0 +1,18 @@
+
+
+public class bluebird {
+    
+    public static void main(String[] args) {
+        greetHello();
+        greetGoodbye();
+    }
+
+    public static void greetHello() {
+        System.out.println("Hello, I am Bluebird");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void greetGoodbye() {
+        System.out.println("Bye");
+    }
+}
