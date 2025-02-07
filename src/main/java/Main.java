6c70db665de448e08b60e64bdb4ff014a5097b6b
zavsky <irwinteo@gmail.com>
2025-02-07 22:26:07 +0800

    Add echo user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
+        String userPrompt;
         bluebird bb = new bluebird();
         bb.greetHello();
+
+        Scanner userPromptScanner = new Scanner(System.in);
+        userPrompt = userPromptScanner.nextLine().trim().toLowerCase();
+
+        while (!userPrompt.equals("bye")) {
+            System.out.println(userPrompt);
+            userPrompt = userPromptScanner.nextLine().trim().toLowerCase();
+        }
+
         bb.greetGoodbye();
+        userPromptScanner.close();
     }
 }
